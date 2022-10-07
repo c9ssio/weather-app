1731d358c5e46df98ad4e2f07aa830b26152b760
Cassio Machado <cassio.noww@gmail.com>
2022-10-07 10:47:30 -0300

    weather-app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5edd14336dmshc8ba2bc59b3b25cp16c1a7jsn4157d18fea54",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5ea3d17db9611f716e43e3e11ce415ad";
